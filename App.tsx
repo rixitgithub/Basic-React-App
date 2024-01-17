654811195b142f922c39f785b8c99e9fbf91cd51
Rishit Tiwari <128586763+rixitgithub@users.noreply.github.com>
2024-01-17 11:08:02 +0530

    Learnt Alert Messages

--- App.tsx
+++ App.tsx
@@ -1,17 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  const items = ["New Delhi", "San Fransisco", "Mumbai", "Tokyo", "Otto"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>{" "}
+      </Alert>
     </div>
   );
 }
